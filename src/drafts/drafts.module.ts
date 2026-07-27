d51ed61c1b5f108c543516469cc712aebca8c30d
JACKSON CHINEDU <chiefjacksonian@gmail.com>
2026-07-27 21:13:05 +0100

    ng

--- src/drafts/drafts.module.ts
+++ src/drafts/drafts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DraftsController } from './drafts.controller';
 import { DraftsService } from './drafts.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TemplatesModule],
   controllers: [DraftsController],
   providers: [DraftsService],
   exports: [DraftsService],
